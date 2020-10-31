63dd2daabea8746d0fea2c7f206252ebcc5f8ce6
baker97-coder <baker.d.andrew@gmail.com>
2020-10-30 20:42:33 -0700

    added css styling

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCTWdNV6RpOquDOmYz0-mPR8XB6qN_3mK4",
+    authDomain: "bike-landing-page.firebaseapp.com",
+    databaseURL: "https://bike-landing-page.firebaseio.com",
+    projectId: "bike-landing-page",
+    storageBucket: "bike-landing-page.appspot.com",
+    messagingSenderId: "435516274887",
+    appId: "1:435516274887:web:0cb2d1012a9e2fb35966e2",
+    measurementId: "G-H6VGD4W7MQ"
+  };
